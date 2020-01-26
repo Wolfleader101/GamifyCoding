ea1a1d3d9d68128d9888a11c419770ed5f9ed7a9
Wolfleader <james.morgan@iinet.net.au>
2020-01-26 15:36:37 +0800

    Database works, it appends name to json

--- db.js
+++ db.js
@@ -1,15 +1,19 @@
 const fs = require('fs');
 const path = require('path');
-
+const DbPath = path.join(__dirname + '/src/database/database.json');
 module.exports = {
     database: (user) => {
-        fs.readFile(path.join(__dirname + '/src/database/database.json'), 'utf-8', (err, data) => {
+        fs.readFile(DbPath, 'utf-8', (err, data) => {
             db = JSON.parse(data);
             db[user] = {
                 XP: 0,
                 Level: 0,
             }
             console.log(db);
+            fs.writeFile(DbPath, JSON.stringify(db), (err) => {
+                console.log(err)
+            });
+            
             
         });
     }
