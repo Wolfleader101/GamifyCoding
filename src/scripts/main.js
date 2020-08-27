3fcb5cedefbf73823a0ea8ad0fa4bc375f557c7d
Wolfleader <james.morgan@iinet.net.au>
2020-08-27 18:31:28 +0800

    adding in UI

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -31,7 +31,7 @@ function stopWatch() {
         if(Number.isInteger(savedUser.XP / 100)) {
            savedUser.Level++
         }
-
+        UpdateUI();
         localStorage.setItem(User, JSON.stringify(savedUser))
 
     }
